8dc26fed1cb29164e6be408bd8cbe5bb4a0b12a3
mohamed rashad <mohamedabdouooo28@gmail.com>
2025-11-09 09:10:02 +0200

    خ

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 // import FloatingAvatarButton from "./components/FloatingAvatarButton";
+import ScrollToTop from "./components/ScrollToTop";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import FirebaseTestPage from "./pages/accounting/FirebaseTestPage";
@@ -27,6 +28,7 @@ const App = () => (
           <Toaster />
           <Sonner />
           <BrowserRouter>
+            <ScrollToTop />
             <SidebarProvider>
               <Routes>
                 <Route path="/*" element={<Index />} />
